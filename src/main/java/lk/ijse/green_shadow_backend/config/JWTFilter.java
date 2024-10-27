a964bb87d11a9215d140e57c115e3963ab6dff1c
KusalDemo <kusalgunasekara2002@gmail.com>
2024-10-27 12:50:54 +0530

    Implemented JWTService and JWTFilter

--- src/main/java/lk/ijse/green_shadow_backend/config/JWTFilter.java
+++ src/main/java/lk/ijse/green_shadow_backend/config/JWTFilter.java
@@ -4,7 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.green_shadow_backend.service.impl.JWTService;
+import lk.ijse.green_shadow_backend.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,8 +20,29 @@ import java.io.IOException;
 
 @Component
 public class JWTFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTService jwtService;
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String auth = request.getHeader("Authorization");
+        String token="";
+        String userEmail="";
 
+        if(auth!=null && auth.startsWith("Bearer ")){
+            token = auth.substring(7);
+            userEmail=jwtService.extractUserEmail(token);
+        }
+        if(userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = applicationContext.getBean(UserServiceImpl.class).loadUserByUsername(userEmail);
+            if(jwtService.validateToken(token, userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
