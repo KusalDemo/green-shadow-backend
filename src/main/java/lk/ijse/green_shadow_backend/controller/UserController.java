6fac37448ba5ab1f5b5e102a98d79dc79f99e96e
KusalDemo <kusalgunasekara2002@gmail.com>
2024-10-27 11:28:15 +0530

    Implemented register user related methods in UserController and UserServiceImpl

--- src/main/java/lk/ijse/green_shadow_backend/controller/UserController.java
+++ src/main/java/lk/ijse/green_shadow_backend/controller/UserController.java
@@ -1,17 +1,31 @@
 package lk.ijse.green_shadow_backend.controller;
 
+import lk.ijse.green_shadow_backend.dto.impl.UserDTO;
+import lk.ijse.green_shadow_backend.entity.impl.User;
+import lk.ijse.green_shadow_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> register() {
-        return null;
+    public ResponseEntity<Void> register(@RequestBody UserDTO userDTO) {
+        try{
+            userService.register(userDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/lk/ijse/green_shadow_backend/service/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/green_shadow_backend/service/impl/UserServiceImpl.java
@@ -1,16 +1,28 @@
 package lk.ijse.green_shadow_backend.service.impl;
 
+import lk.ijse.green_shadow_backend.dao.UserDAO;
 import lk.ijse.green_shadow_backend.dto.impl.UserDTO;
 import lk.ijse.green_shadow_backend.entity.impl.User;
 import lk.ijse.green_shadow_backend.secure.JWTAuthResponse;
 import lk.ijse.green_shadow_backend.service.UserService;
+import lk.ijse.green_shadow_backend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDAO userDao;
+    @Autowired
+    private Mapping mapper;
+
+    private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
+
     @Override
     public User register(UserDTO userDTO) {
-        return null;
+        userDTO.setPassword(encoder.encode(userDTO.getPassword()));
+        return userDao.save(mapper.mapToUser(userDTO));
     }
 
     @Override
